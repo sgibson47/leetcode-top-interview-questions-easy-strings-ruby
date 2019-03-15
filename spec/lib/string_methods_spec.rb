0d0a339e013a91eb688171365a3cf2de349c63be
sgibson47 <sgibson47@gmail.com>
2019-03-15 13:23:47 -0500

    add spec for #reverse

--- spec/lib/string_methods_spec.rb
+++ spec/lib/string_methods_spec.rb
@@ -1,2 +1,17 @@
 require 'spec_helper'
-# require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-strings-easy-ruby/lib/string_methods.rb'
+require '/Users/samanthagibson/Development/code/leetcode-top-interview-questions-strings-easy-ruby/lib/string_methods.rb'
+
+describe "#reverse" do 
+  describe "Example 1" do
+    it "returns reversed string" do 
+      string = ["h","e","l","l","o"]
+      expect(reverse(string)).to eq(["o","l","l","e","h"])
+    end
+  end
+  describe "Example 2" do
+    it "returns reversed string" do 
+      string = ["H","a","n","n","a","h"]
+      expect(reverse(string)).to eq(["h","a","n","n","a","H"])
+    end
+  end
+end
