7e0fa86ce289fe9e9526e2b4f19ca84b17b09227
sgibson47 <sgibson47@gmail.com>
2019-03-15 16:41:07 -0500

    add alternate solutions for #reverse

--- lib/string_methods.rb
+++ lib/string_methods.rb
@@ -1,14 +1,38 @@
 def reverse_array(s)
   #s.reverse!
+  
   # without reverse!
+  # return s if s.length <=1
+  # output =[]
+  # i = s.length-1
+  # while i >= 0
+  #   output.push(s[i])
+  #   i -=1
+  # end
+  # output
+
+  # but chaning the array in place
   return s if s.length <=1
-  output =[]
   i = s.length-1
+  output = []
   while i >= 0
-    output.push(s[i])
+    output.push(s.pop)
     i -=1
   end
-  output
+  
+  output.map {|elem| s.push(elem)}
+  s
+
+  # or 
+    #swap(s,0,s.length-1)
+end
+
+def swap(s, l, r)
+  return s if l>=r
+  temp = s[l]
+  s[l] = s[r]
+  s[r] = temp
+  swap s,l+1,r-1
 end
 
 def reverse(s)
@@ -18,7 +42,7 @@ def reverse(s)
   return s if s.length <=1
   output =[]
   array = s.chars
-  i = s.length-1
+  i = s.length
   while i >= 0
     output.push(array[i])
     i -=1
