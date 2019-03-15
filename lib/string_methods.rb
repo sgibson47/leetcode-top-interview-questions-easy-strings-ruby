ab0f27beea5debbaf031802764b90d6859934f16
sgibson47 <sgibson47@gmail.com>
2019-03-15 13:49:34 -0500

    add solution for #reverse

--- lib/string_methods.rb
+++ lib/string_methods.rb
@@ -1,3 +1,27 @@
 def reverse_array(s)
-  s.reverse!
+  #s.reverse!
+  # without reverse!
+  return s if s.length <=1
+  output =[]
+  i = s.length-1
+  while i >= 0
+    output.push(s[i])
+    i -=1
+  end
+  output
+end
+
+def reverse(s)
+  #s.chars.reverse!.join('')
+  #without reverse!
+
+  return s if s.length <=1
+  output =[]
+  array = s.chars
+  i = s.length-1
+  while i >= 0
+    output.push(array[i])
+    i -=1
+  end
+  output.join('')
 end
